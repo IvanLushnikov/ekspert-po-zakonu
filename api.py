0b93630438435bb74b0fa69676c37a6c24b2199f
IvanLushnikov <ivanlusnikov159@gmail.com>
2025-04-13 00:00:24 +0500

    Update api.py

--- api.py
+++ api.py
@@ -9,7 +9,13 @@ from pathlib import Path
 from numpy.linalg import norm
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
+@app.after_request
+def apply_cors(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
